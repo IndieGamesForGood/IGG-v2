6af75a776062fb6ecf9d8b36dc359f4d1862daad
Matthew Rasmussen <mrasmus@gmail.com>
2012-05-21 01:33:56 -0700

    Fixed broken models.

--- src/igg/marathon/models.py
+++ src/igg/marathon/models.py
@@ -6,19 +6,40 @@ from django.utils.translation import ugettext as _
 # https://docs.djangoproject.com/en/dev/topics/i18n/translation/
 
 
-class Challenge(models.Model):
-  pass
 
+class Game(models.Model):
+  name = models.CharField(max_length=200)
+  developer = models.CharField(max_length=200)
+  site = models.CharField(max_length=200)
+  description = models.TextField()
+  approved = models.BooleanField(default=False)
+
+class Challenge(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.TextField()
+  accepted = models.BooleanField(default=False)
+  private = models.BooleanField(default=False)
+  bounty = models.DecimalField(max_digits=14, decimal_places=2)
+  user = models.ForeignKey(User)
+
+
+class Raffle(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.TextField(max_length=500)
+  quantity = models.IntegerField(default=1)
+  public = models.BooleanField(default=False)
+  start = models.DateTimeField()
+  end = models.DateTimeField()
 
 class Donation(models.Model):
   user = models.ForeignKey(User)
   amount = models.DecimalField(max_digits=14, decimal_places=2, help_text=_(u'Maximum $999,999,999,999.99'))
-  comment = models.CharField()
-  paypalResponse = models.CharField()
+  comment = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
   game = models.ForeignKey(Game)
   challenge = models.ForeignKey(Challenge)
   raffle = models.ForeignKey(Raffle)
+  confirmed = models.BooleanField(default=False)
 
   def __unicode__(self):
     import locale
@@ -27,40 +48,15 @@ class Donation(models.Model):
       {'amount': locale.currency(self.amount, grouping=True),
        'user': self.user.__unicode__()}
 
-
-class Game(models.Model):
-  name = models.CharField(max_length=200)
-  developer = models.CharField(max_length=200)
-  site = models.CharField(max_length=200)
-  description = models.CharField(max_length=500)
-  approved = models.BooleanField(default=False)
-
-class Challenge(models.Model):
-  name = models.CharField(max_length=200)
-  description = models.CharField(max_length=500)
-  accepted = models.BooleanField(default=False)
-  private = models.BooleanField(default=False)
-  bounty = models.DecimalField(decimal_places=2)
-  user = models.ForeignKey(User)
-
-
-class Raffle(models.Model):
-  name = models.CharField(max_length=200)
-  description = models.CharField(max_length=1000)
-  quantity = models.IntegerField(default=1)
-  public = models.BooleanField(default=False)
-  start = models.DateTimeField()
-  end = models.DateTimeField()
-
 class PointTransaction(models.Model):
-  user = models.ForeignKey(uauthmodels.User)
+  user = models.ForeignKey(User)
   game = models.ForeignKey(Game)
   amount = models.IntegerField()
   spent = models.IntegerField()
   timestamp = models.DateTimeField(auto_now_add=True)
 
 class RaffleEntry(models.Model):
-  user = models.ForeignKey(uauthmodels.User)
+  user = models.ForeignKey(User)
   raffle = models.ForeignKey(Raffle)
   tickets = models.IntegerField()
   timestamp = models.DateTimeField(auto_now_add=True)
