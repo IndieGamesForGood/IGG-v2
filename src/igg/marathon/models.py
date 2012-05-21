e9ed4c901d65110268c93898a113ae0ebe20d258
Matthew Rasmussen <mrasmus@gmail.com>
2012-05-21 00:47:25 -0700

    Fleshed out models.

--- src/igg/marathon/models.py
+++ src/igg/marathon/models.py
@@ -13,6 +13,12 @@ class Challenge(models.Model):
 class Donation(models.Model):
   user = models.ForeignKey(User)
   amount = models.DecimalField(max_digits=14, decimal_places=2, help_text=_(u'Maximum $999,999,999,999.99'))
+  comment = models.CharField()
+  paypalResponse = models.CharField()
+  timestamp = models.DateTimeField(auto_now_add=True)
+  game = models.ForeignKey(Game)
+  challenge = models.ForeignKey(Challenge)
+  raffle = models.ForeignKey(Raffle)
 
   def __unicode__(self):
     import locale
@@ -24,10 +30,40 @@ class Donation(models.Model):
 
 class Game(models.Model):
   name = models.CharField(max_length=200)
+  developer = models.CharField(max_length=200)
+  site = models.CharField(max_length=200)
+  description = models.CharField(max_length=500)
+  approved = models.BooleanField(default=False)
+
+class Challenge(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.CharField(max_length=500)
+  accepted = models.BooleanField(default=False)
+  private = models.BooleanField(default=False)
+  bounty = models.DecimalField(decimal_places=2)
+  user = models.ForeignKey(User)
 
 
 class Raffle(models.Model):
   name = models.CharField(max_length=200)
+  description = models.CharField(max_length=1000)
+  quantity = models.IntegerField(default=1)
+  public = models.BooleanField(default=False)
+  start = models.DateTimeField()
+  end = models.DateTimeField()
+
+class PointTransaction(models.Model):
+  user = models.ForeignKey(uauthmodels.User)
+  game = models.ForeignKey(Game)
+  amount = models.IntegerField()
+  spent = models.IntegerField()
+  timestamp = models.DateTimeField(auto_now_add=True)
+
+class RaffleEntry(models.Model):
+  user = models.ForeignKey(uauthmodels.User)
+  raffle = models.ForeignKey(Raffle)
+  tickets = models.IntegerField()
+  timestamp = models.DateTimeField(auto_now_add=True)
 
 
 class UserProfile(models.Model):
