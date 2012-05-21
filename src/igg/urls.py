c26046a14d07af9f6fb11cd9c7ccc716e2024c86
Brandon Carl <bcarl@me.com>
2012-05-20 20:32:07 -0700

    initial template commit

--- src/igg/urls.py
+++ src/igg/urls.py
@@ -1,20 +1,18 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'igg.views.home', name='home'),
-    # url(r'^igg/', include('igg.foo.urls')),
+  # Admin
+  url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+  url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+  url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
 )
 
 # https://docs.djangoproject.com/en/dev/howto/static-files/#serving-static-files-in-development
