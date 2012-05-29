3373fe2c48f9b5d37b8522d1deb90c471714b890
Matthew Rasmussen <mrasmus@gmail.com>
2012-05-29 04:46:54 -0700

    Foundational transaction system built.

--- src/igg/marathon/models.py
+++ src/igg/marathon/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
@@ -18,11 +19,12 @@ class Game(models.Model):
   def __unicode__(self):
     import locale
     locale.setlocale(locale.LC_ALL, '')
-    return _(u'Game: %(name)s') %\
-           {'name': self.name}
+    return _(u'Game: %(name)s%(status)s') %\
+           {'name': self.name,
+            'status': (' (NOT VISIBLE)' if not self.visible else '')}
 
   class Meta:
-    ordering = ['name']
+    ordering = ['visible','name']
 
 class Challenge(models.Model):
   name = models.CharField(max_length=200)
@@ -30,27 +32,43 @@ class Challenge(models.Model):
   accepted = models.BooleanField(default=False)
   private = models.BooleanField(default=False)
   bounty = models.DecimalField(max_digits=14, decimal_places=2, null=True, blank=True)
+  total = models.DecimalField(max_digits=14, decimal_places=2, default=0.00, null=True, blank=True)
   user = models.ForeignKey(User)
   slug = models.SlugField()
+
+  def __unicode__(self):
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+    return _(u'Challenge: %(name)s (%(status)s%(privacy)s)') %\
+         {'name': self.name,
+          'status': ('NOT ' if not self.accepted else '') + 'Accepted',
+          'privacy':(', PRIVATE: ' + self.user.__unicode__() if self.private else '')}
 
 class Raffle(models.Model):
   name = models.CharField(max_length=200)
   description = models.TextField(max_length=500)
   quantity = models.IntegerField(default=1)
   visible = models.BooleanField(default=False)
-  start = models.DateTimeField()
-  end = models.DateTimeField()
+  start = models.DateTimeField(null=True, blank=True)
+  end = models.DateTimeField(null=True, blank=True)
   slug = models.SlugField()
 
+  def __unicode__(self):
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+    return _(u'Raffle: %(name)s%(status)s') %\
+         {'name': self.name,
+          'status': (' (HIDDEN)' if not self.visible else '')}
+
 class Donation(models.Model):
-  user = models.ForeignKey(User)
+  user = models.ForeignKey(User, related_name='donations')
   amount = models.DecimalField(max_digits=14, decimal_places=2, help_text=_(u'Maximum $999,999,999,999.99'))
-  comment = models.TextField()
+  comment = models.TextField(null=True, blank=True)
   timestamp = models.DateTimeField(auto_now_add=True)
-  game = models.ForeignKey(Game)
-  challenge = models.ForeignKey(Challenge)
-  raffle = models.ForeignKey(Raffle)
-  confirmed = models.BooleanField(default=False)
+  game = models.ForeignKey(Game, null=True, blank=True)
+  challenge = models.ForeignKey(Challenge, null=True, blank=True, related_name='donations')
+  raffle = models.ForeignKey(Raffle, null=True, blank=True)
+  approved = models.BooleanField(default=False)
 
   def __unicode__(self):
     import locale
@@ -60,26 +78,39 @@ class Donation(models.Model):
        'user': self.user.__unicode__()}
 
 class PointTransaction(models.Model):
-  user = models.ForeignKey(User)
+  user = models.ForeignKey(User, related_name='transactions')
   game = models.ForeignKey(Game)
   amount = models.IntegerField()
   spent = models.IntegerField()
   timestamp = models.DateTimeField(auto_now_add=True)
 
+  def __unicode__(self):
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+    return _(u'PointTransaction:  %(name)s %(type)s %(count)sT%(game)s') %\
+         {'type': 'GEN' if self.game.id==1 else ( 'PUT' if self.amount > 0 else 'GET'),
+          'count': self.amount,
+          'name': self.user.__unicode__(),
+          'game': '' if self.game.id==1 else ' | ' + self.game.name}
+
 class RaffleEntry(models.Model):
-  user = models.ForeignKey(User)
+  user = models.ForeignKey(User, related_name='entries')
   raffle = models.ForeignKey(Raffle)
   tickets = models.IntegerField()
   timestamp = models.DateTimeField(auto_now_add=True)
 
 class Schedule(models.Model):
-  start = models.DateTimeField();
-  end = models.DateTimeField();
-  game = models.ForeignKey(Game);
+  start = models.DateTimeField()
+  end = models.DateTimeField()
+  game = models.ForeignKey(Game, null=True, blank=True, related_name='schedules')
+  title = models.CharField(max_length=200, null=True, blank=True)
+  description = models.TextField(max_length=500, null=True, blank=True)
   visible = models.BooleanField(default=False)
 
 class UserProfile(models.Model):
   user = models.OneToOneField(User)
+  tickets = models.IntegerField(default=0)
+  points = models.IntegerField(default=0)
 
   def __unicode__(self):
     return self.user.__unicode__()
@@ -87,3 +118,31 @@ class UserProfile(models.Model):
 # http://www.turnkeylinux.org/blog/django-profile
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
+
+# Signal handlers: https://docs.djangoproject.com/en/dev/topics/signals/#connecting-receiver-functions
+@receiver(models.signals.pre_save,sender=Donation)
+def donationSaving(sender, instance, **kwargs):
+  try:
+    if not Donation.objects.get(pk=instance.pk).approved and instance.approved:
+      instance.user.profile.points += 500 #TODO: Remove these manual manipulations and put into Transactions
+      instance.user.profile.tickets += 5  #TODO: Remove (see above)
+      npt = PointTransaction(user=instance.user, game=Game.objects.get(id='1'), amount=-500, spent=0)
+      npt.save()
+      nre = RaffleEntry(user=instance.user, raffle=Raffle.objects.get(id='1'), tickets=-5)
+      nre.save()
+      if (instance.game):
+        instance.user.profile.points -= 500  #TODO: Remove (see above)
+        pt = PointTransaction(user=instance.user, game=instance.game, amount=500, spent=0)
+        pt.save()
+      if (instance.raffle):
+        instance.user.profile.tickets += 5    #TODO: Remove (see above)
+        re = RaffleEntry(user=instance.user, raffle=instance.raffle, tickets=5)
+        re.save()
+  except Donation.DoesNotExist:
+    pass
+
+@receiver(models.signals.post_save,sender=Donation)
+def donationSaved(sender, instance, **kwargs):
+  if (instance.challenge):
+    instance.challenge.total = sum(foo.amount for foo in instance.challenge.donations.filter(approved=True))
+    instance.challenge.save()
