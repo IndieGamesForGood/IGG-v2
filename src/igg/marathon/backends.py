09b39bd9e66b066cbeae9f9756d8f92ff37c5560
Brandon Carl <bcarl@me.com>
2012-05-29 02:59:02 -0700

    Fixing user registration to ignore usernames and only use emails for logging in and creating users.

--- src/igg/marathon/backends.py
+++ src/igg/marathon/backends.py
@@ -1,13 +1,25 @@
-"""
-Custom Authentication backend that supports using an e-mail address
-to login instead of a username.
+import time
 
-See: http://blog.cingusoft.org/custom-django-authentication-backend
-"""
-
+from django.conf import settings
 from django.contrib.auth.models import User, check_password
+from django.contrib.sites.models import RequestSite
+from django.contrib.sites.models import Site
+from django.db import transaction
+
+from igg.marathon.forms import NoUsernameRegistrationForm
+
+from registration.backends.default import DefaultBackend
+from registration import signals
+from registration.models import RegistrationProfile
+
 
 class LoginUsingEmailAsUsernameBackend(object):
+  """
+  Custom Authentication backend that supports using an e-mail address
+  to login instead of a username.
+
+  See: http://blog.cingusoft.org/custom-django-authentication-backend
+  """
   supports_object_permissions = False
   supports_anonymous_user = False
   supports_inactive_user = False
@@ -30,3 +42,36 @@ class LoginUsingEmailAsUsernameBackend(object):
       return User.objects.get(pk=user_id)
     except User.DoesNotExist:
       return None
+
+class IggRegistrationBackend(DefaultBackend):
+
+  def register(self, request, **kwargs):
+    email, password, \
+    first_name, last_name = kwargs['email'], kwargs['password1'], \
+                            kwargs['first_name'], kwargs['last_name']
+
+    # Username is the current time plus part of the user's email, all truncated to 30 chars.
+    username = (str(time.time()) + '_' + email.replace('@', '').replace('.', '').replace('_', ''))[:30]
+
+    if Site._meta.installed:
+      site = Site.objects.get_current()
+    else:
+      site = RequestSite(request)
+
+    new_user = new_user = User.objects.create_user(username, email, password)
+    new_user.is_active = False
+    new_user.first_name = first_name
+    new_user.last_name = last_name
+    new_user.save()
+    
+    registration_profile = RegistrationProfile.objects.create_profile(new_user)
+    registration_profile.send_activation_email(site)
+
+    signals.user_registered.send(sender=self.__class__,
+                                 user=new_user,
+                                 request=request)
+    return new_user
+  register = transaction.commit_on_success(register)
+
+  def get_form_class(self, request):
+    return NoUsernameRegistrationForm
--- src/igg/urls.py
+++ src/igg/urls.py
@@ -2,8 +2,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.views.generic.simple import direct_to_template
 
 from igg.marathon.views import *
+
+from registration.views import activate
+from registration.views import register
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,7 +19,29 @@ urlpatterns = patterns('',
   url(r'^admin/', include(admin.site.urls)),
   
   # django-registration
-  url(r'', include('registration.backends.default.urls')),
+  url(r'^activate/complete/$',
+    TemplateView.as_view(template_name='registration/activation_complete.html'),
+    name='registration_activation_complete'),
+  # Activation keys get matched by \w+ instead of the more specific
+  # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+  # that way it can return a sensible "invalid key" message instead of a
+  # confusing 404.
+  url(r'^activate/(?P<activation_key>\w+)/$',
+    activate,
+    {'backend': 'igg.marathon.backends.IggRegistrationBackend'},
+    name='registration_activate'),
+  url(r'^register/$',
+    register,
+    {'backend': 'igg.marathon.backends.IggRegistrationBackend'},
+    name='registration_register'),
+  url(r'^register/complete/$',
+    direct_to_template,
+    {'template': 'registration/registration_complete.html'},
+    name='registration_complete'),
+  url(r'^register/closed/$',
+    TemplateView.as_view(template_name='registration/registration_closed.html'),
+    name='registration_disallowed'),
+  url(r'', include('registration.auth_urls')),
 
   url(r'^$', TemplateView.as_view(template_name='marathon/index.html'), name='home'),
   url(r'^donate-now/$', TemplateView.as_view(template_name='marathon/donate.html'), name='donate_now'),
