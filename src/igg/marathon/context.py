d2f32f1174be51c0ac9fc01c22b19e66e9c3391d
Brandon Carl <bcarl@me.com>
2012-05-31 00:54:30 -0700

    adding constants to template context processor

--- src/igg/marathon/context.py
+++ src/igg/marathon/context.py
@@ -6,4 +6,10 @@ def marathon_info(request):
 
   Adds the ``marathon`` object to the template context.
   """
-  return {'marathon': MarathonInfo.objects.get(pk=settings.IGG_PARAM_MARATHONINFO_PK)}
+  return {
+    'marathon': MarathonInfo.objects.get(pk=settings.IGG_PARAM_MARATHONINFO_PK),
+    'IGG_PARAM_RATE': settings.IGG_PARAM_RATE,
+    'IGG_PARAM_I_HR_COST': settings.IGG_PARAM_I_HR_COST,
+    'IGG_PARAM_DOLLARS_PER_TICKET': settings.IGG_PARAM_DOLLARS_PER_TICKET,
+    'IGG_PARAM_PTS_PER_MIN': settings.IGG_PARAM_PTS_PER_MIN
+  }
